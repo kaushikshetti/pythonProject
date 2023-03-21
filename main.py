1bf139db22b2b88f3d14613cb37075592583040d
Kaushik Shetti <kshetti29@gmail.com>
2023-03-21 22:39:40 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+def add_num(a,b):#function for addition
+    sum=a+b;
+    return sum; #return value
+num1=25  #variable declaration
+num2=59
+print("The sum is",add_num(num1,num2))#call the function
